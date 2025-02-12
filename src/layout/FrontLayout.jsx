5ca7e91d39b3b0285e87166122fe2bd019bfe112
Alice <Alice@c-link.com.tw>
2025-02-12 16:48:16 +0800

    1. Add react-week6.

---
+++ src/layout/FrontLayout.jsx
@@ -0,0 +1,26 @@
+import { NavLink, Outlet } from "react-router-dom"
+const routes = [
+  { path: "/", name: "首頁" },
+  { path: "/products", name: "產品列表" },
+  { path: "/cart", name: "購物車" },
+  { path: "/login", name: "登入頁" },
+];
+export default function FrontLayout() {
+  return (
+    <>
+      <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div className="container">
+          <ul className="navbar-nav flex-row gap-5 fs-5">
+            {routes.map((route)=> (
+              <li className="nav-item" key={route.path}>
+                <NavLink className="nav-link" aria-current="page" to={route.path}>{ route.name }</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+ 
+  )
+}
---
+++ src/router/index.jsx
@@ -0,0 +1,43 @@
+import { createHashRouter } from "react-router-dom"
+import FrontLayout from "../layout/FrontLayout"
+import HomePage from "../pages/HomePage"
+import ProductsPage from "../pages/ProductsPage"
+import CartPage from "../pages/CartPage"
+import ProductDetailPage from "../pages/ProductDetailPage"
+import NotFound from "../pages/NotFound"
+import LoginPage from "../pages/LoginPage"
+
+const router = createHashRouter([
+  {
+    path:'/',
+    element: <FrontLayout/>,
+    children: [
+      {
+        path:'',
+        element: <HomePage/>,
+      },
+      {
+        path:'/products',
+        element: <ProductsPage/>,
+      },
+      {
+        path:'/products/:id',
+        element: <ProductDetailPage/>,
+      },
+      {
+        path:'/cart',
+        element: <CartPage/>,
+      }
+    ]
+  },
+  {
+    path:'*',
+    element: <NotFound/>
+  },
+  {
+    path:'/login',
+    element: <LoginPage/>
+  }
+])
+
+export default router
